1ef17e75e139120259e7d05b67c5b238d5554bd5
luo00010 <1851298930@qq.com>
2023-09-15 13:39:49 +0800

    新增角色的增删改查功能

--- service-oa/src/main/java/com/luo/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/luo/auth/ServiceAuthApplication.java
@@ -4,9 +4,10 @@ package com.luo.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.luo.auth.mapper")
+@ComponentScan("com.luo")
 public class ServiceAuthApplication {
 
     public static void main(String[] args) {
--- service-oa/src/main/java/com/luo/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/luo/auth/controller/SysRoleController.java
@@ -1,15 +1,19 @@
 package com.luo.auth.controller;
 
 import com.atguigu.model.system.SysRole;
+import com.atguigu.vo.system.SysRoleQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.luo.auth.service.SysRoleService;
 import com.luo.common.result.Result;
+import com.mysql.cj.log.Log;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,13 +42,75 @@ public class SysRoleController {
         return Result.ok(list);
     }
 
-    //条件分页查询
-
-
     //添加角色
+    @ApiOperation("添加角色")
+    @PostMapping("save")
+    public Result save(@RequestBody SysRole sysRole){
+        boolean is_success = sysRoleService.save(sysRole);
+        if (!is_success){
+            return Result.fail();
+        }
+        return Result.ok();
+    }
 
     //删除角色
+    @ApiOperation(value = "删除角色")
+    @DeleteMapping("remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        sysRoleService.removeById(id);
+        return Result.ok();
+    }
 
+    //修改角色
+    @ApiOperation(value = "修改角色")
+    @PutMapping("update")
+    public Result updateById(@RequestBody SysRole sysRole){
+        sysRoleService.updateById(sysRole);
+        return Result.ok();
+    }
+
+
+    //根据id列表删除
+    @ApiOperation(value = "根据id列表删除")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> idList) {
+        sysRoleService.removeByIds(idList);
+        return Result.ok();
+    }
+
+
+
+    //条件分页查询
+    /**
+     *
+     * @param page   当前页数
+     * @param limit     每页现实记录数
+     * @param sysRoleQueryVo    条件对象
+     * @return
+     */
+    @ApiOperation("条件分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable Long limit,
+                                SysRoleQueryVo sysRoleQueryVo){
+
+        //调用service的方法实现
+        //1 创建Page对象，传递分页相关参数
+        Page<SysRole> pageParam = new Page<>(page,limit);
+
+        //2 封装条件，判断条件是否为空，不为空进行包装
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if (!StringUtils.isEmpty(roleName)){
+            //封装
+            wrapper.like(SysRole::getRoleName,roleName);
+        }
+
+        //调用方法实现
+        IPage<SysRole> pageModel = sysRoleService.page(pageParam, wrapper);
+
+        return Result.ok(pageModel);
+    }
 
 
 
